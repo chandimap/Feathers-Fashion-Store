592473130af26f8297a5c830bf6e645bca1ab8f5
Chandima Ranasinghe <chandimapr@gmail.com>
2020-08-25 19:36:27 +0530

    Updates

---
+++ precache-manifest.ee0864b977faeba00c93f2ca73913308.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6160c2401992d8b19eb",
+    "url": "/Feathers-Fashion-Store/static/css/main.dede34a2.chunk.css"
+  },
+  {
+    "revision": "c6160c2401992d8b19eb",
+    "url": "/Feathers-Fashion-Store/static/js/main.c6160c24.chunk.js"
+  },
+  {
+    "revision": "b1ada6228eb8af09fbe6",
+    "url": "/Feathers-Fashion-Store/static/css/1.ddfced4d.chunk.css"
+  },
+  {
+    "revision": "b1ada6228eb8af09fbe6",
+    "url": "/Feathers-Fashion-Store/static/js/1.b1ada622.chunk.js"
+  },
+  {
+    "revision": "6d9c3da4f29b48eff7a3",
+    "url": "/Feathers-Fashion-Store/static/css/2.e59f84b9.chunk.css"
+  },
+  {
+    "revision": "6d9c3da4f29b48eff7a3",
+    "url": "/Feathers-Fashion-Store/static/js/2.6d9c3da4.chunk.js"
+  },
+  {
+    "revision": "92582336101288aa072e",
+    "url": "/Feathers-Fashion-Store/static/js/3.92582336.chunk.js"
+  },
+  {
+    "revision": "629c0a54621342d781e1",
+    "url": "/Feathers-Fashion-Store/static/js/runtime~main.629c0a54.js"
+  },
+  {
+    "revision": "0c8b9f5376870f6e8d633dc4d6e572ab",
+    "url": "/Feathers-Fashion-Store/static/media/logo.0c8b9f53.png"
+  },
+  {
+    "revision": "1bf6b78f3f88ce2b534c6a26ec659a64",
+    "url": "/Feathers-Fashion-Store/static/media/heart_white.1bf6b78f.png"
+  },
+  {
+    "revision": "f2200d0cabff3072915b4125e6867912",
+    "url": "/Feathers-Fashion-Store/static/media/cart_white.f2200d0c.png"
+  },
+  {
+    "revision": "92a6cf67bedfa14ee1445accb0114812",
+    "url": "/Feathers-Fashion-Store/static/media/fb.92a6cf67.png"
+  },
+  {
+    "revision": "5be6ee4be4b6d62fd8cb6b2e8e1f65fe",
+    "url": "/Feathers-Fashion-Store/static/media/instagram.5be6ee4b.png"
+  },
+  {
+    "revision": "289716e2e3c9c2c6ce36de8357d9e010",
+    "url": "/Feathers-Fashion-Store/static/media/pinterest.289716e2.png"
+  },
+  {
+    "revision": "0deebf8573a205c14fa678247a5edc1d",
+    "url": "/Feathers-Fashion-Store/static/media/twitter.0deebf85.png"
+  },
+  {
+    "revision": "3da22560ee1686c2e28b0f30e9196ed0",
+    "url": "/Feathers-Fashion-Store/static/media/Sale.3da22560.jpg"
+  },
+  {
+    "revision": "1ad5c71f91bfc87f5be3f25d45f12539",
+    "url": "/Feathers-Fashion-Store/static/media/slide_1.1ad5c71f.jpg"
+  },
+  {
+    "revision": "8bfe4b93c5aa4c03639ec33de40987ba",
+    "url": "/Feathers-Fashion-Store/static/media/slide_2.8bfe4b93.jpg"
+  },
+  {
+    "revision": "2f7f5a2501a9dcdf3b285be484f0855e",
+    "url": "/Feathers-Fashion-Store/static/media/slide_3.2f7f5a25.jpg"
+  },
+  {
+    "revision": "a9388fbcd875f25b18790d955cd6fa9c",
+    "url": "/Feathers-Fashion-Store/static/media/slide_4.a9388fbc.jpg"
+  },
+  {
+    "revision": "6c1ad70a79020778214912cf08d80d04",
+    "url": "/Feathers-Fashion-Store/static/media/slide_5.6c1ad70a.jpg"
+  },
+  {
+    "revision": "de0de0633d9f43532903d4b968854272",
+    "url": "/Feathers-Fashion-Store/index.html"
+  }
+];
